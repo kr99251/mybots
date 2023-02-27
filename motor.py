c021f856cee37dcb34540cd270da7dae72bef0ed
kr99251 <kelly@robinett.org>
2023-02-27 13:06:42 -0500

    robot w/ neural network not moving

--- motor.py
+++ motor.py
@@ -6,25 +6,11 @@ import constants as c
 class MOTOR:
     def __init__(self, jointName) -> None:
         self.jointName = jointName
-        self.Prepare_To_Act()
 
-    def Prepare_To_Act(self):
-        self.motorValues = numpy.linspace(0, 2*numpy.pi, 1000)
-        self.amplitude = c.amplitude
-        if self.jointName == 'Torso_BackLeg':
-            self.frequency = c.frequency
-        else:
-            self.frequency = c.frequency/2
-        self.offset = c.offset
-        self.motorValues = self.amplitude * numpy.sin(self.frequency * self.motorValues + self.offset)
-
-    def Set_Value(self, robot, x):
+    def Set_Value(self, robot, desiredAngle):
         pyrosim.Set_Motor_For_Joint(
             bodyIndex = robot,
             jointName = self.jointName,
             controlMode = p.POSITION_CONTROL,
-            targetPosition = self.motorValues[x],
+            targetPosition = desiredAngle,
             maxForce = 100)
-
-    def Save_Values(self):
-        numpy.save("data/" + self.jointName + ".npy", self.motorValues)
--- robot.py
+++ robot.py
@@ -27,8 +27,11 @@ class ROBOT:
             self.motors[jointName] = MOTOR(jointName)
     
     def Act(self, x):
-        for motor in self.motors:
-            self.motors[motor].Set_Value(self.robotId, x)
+        for neuronName in self.nn.Get_Neuron_Names():
+            if self.nn.Is_Motor_Neuron(neuronName):
+                jointName = self.nn.Get_Motor_Neurons_Joint(neuronName)
+                desiredAngle = self.nn.Get_Value_Of(neuronName)
+                self.motors[jointName].Set_Value(self.robotId, desiredAngle)
 
     def Think(self):
         self.nn.Update()
